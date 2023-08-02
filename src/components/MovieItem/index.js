20c30dfc69d64f0387a846e72212d2d3368fc885
jeyaseelan windows 10 <jeyaseelan.work@gmail.com>
2023-08-02 15:54:53 +0530

    code finalaized

--- src/components/MovieItem/index.js
+++ src/components/MovieItem/index.js
@@ -1,1 +1,41 @@
-// Write your code here
+import Popup from 'reactjs-popup'
+import ReactPlayer from 'react-player'
+import {IoMdClose} from 'react-icons/io'
+
+import './index.css'
+
+const MovieItem = props => {
+  const {movieDetails} = props
+  const {thumbnailUrl, videoUrl} = movieDetails
+
+  const renderPopupContent = close => (
+    <>
+      <button
+        type="button"
+        data-testid="closeButton"
+        className="close-button"
+        onClick={close}
+      >
+        <IoMdClose className="close-icon" />
+      </button>
+      <div className="player-container">
+        <ReactPlayer url={videoUrl} controls width="100%" />
+      </div>
+    </>
+  )
+
+  return (
+    <div className="movie-item-container">
+      <Popup
+        modal
+        trigger={
+          <img src={thumbnailUrl} alt="thumbnail" className="thumbnail-image" />
+        }
+      >
+        {close => renderPopupContent(close)}
+      </Popup>
+    </div>
+  )
+}
+
+export default MovieItem
--- src/components/PrimeVideo/index.js
+++ src/components/PrimeVideo/index.js
@@ -1,1 +1,31 @@
-// Write your code here
+import MoviesSlider from '../MoviesSlider'
+
+import './index.css'
+
+const PrimeVideo = props => {
+  const {moviesList} = props
+
+  const getActionMoviesList = () =>
+    moviesList.filter(eachmovie => eachmovie.categoryId === 'ACTION')
+
+  const getComedyMoviesList = () =>
+    moviesList.filter(eachmovie => eachmovie.categoryId === 'COMEDY')
+
+  return (
+    <div className="prime-video-container">
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/prime-video-img.png"
+        alt="prime video"
+        className="prime-video-image"
+      />
+      <div className="prime-video-movies-container">
+        <h1 className="movie-category-title">Action Movies</h1>
+        <MoviesSlider moviesList={getActionMoviesList()} />
+        <h1 className="movie-category-title">Comedy Movies</h1>
+        <MoviesSlider moviesList={getComedyMoviesList()} />
+      </div>
+    </div>
+  )
+}
+
+export default PrimeVideo
